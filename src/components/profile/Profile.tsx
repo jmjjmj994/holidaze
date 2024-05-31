e441b69d359b7e2a6508542a8d3780adac76b591
jmjjmj <jonathan.mjos@outlook.com>
2024-05-31 13:32:24 +0200

    added some logic for dropdown on profile

--- src/components/profile/Profile.tsx
+++ src/components/profile/Profile.tsx
@@ -1,10 +1,105 @@
 import { User } from 'phosphor-react';
-import { ProfileModal } from './ProfileModal';
-export const Profile = () => (
-  <section className="relative flex items-center bg-orange-500">
-    <button>
-      <User size={25} />
-    </button>
-    <ProfileModal />
-  </section>
-);
+import { useCallback, useEffect, useRef, useState } from 'react';
+type ProfileModalProps = {
+  isOpen: boolean;
+  handleIsClosed: () => void;
+};
+
+export const ProfileModal: React.FC<ProfileModalProps> = ({
+  isOpen,
+  handleIsClosed,
+}) => {
+  const [listIndex, setListIndex] = useState(0);
+  const ref = useRef(null);
+  const menuListRef = useRef<HTMLUListElement | null>(null);
+  const menuListItems = Array.from(
+    menuListRef?.current?.querySelectorAll('li') || []
+  ) as HTMLLIElement[];
+
+  useEffect(() => {
+    if (!isOpen) return;
+    const handleKeyEvent = (e: KeyboardEvent) => {
+      if (e.key === 'Escape') {
+        handleIsClosed();
+      }
+    };
+    window.addEventListener('keydown', handleKeyEvent);
+
+    return () => window.removeEventListener('keydown', handleKeyEvent);
+  }, [isOpen, handleIsClosed]);
+
+  useEffect(() => {
+    if (!isOpen) return;
+    const handleListIndex = (e: KeyboardEvent) => {
+      if (e.key === 'Tab') {
+        setListIndex(listIndex + 1);
+      }
+      if (menuListItems.length - 1 + 1 === listIndex) {
+        handleIsClosed();
+        setListIndex(0);
+      }
+    };
+    window.addEventListener('keydown', handleListIndex);
+    return () => window.removeEventListener('keydown', handleListIndex);
+  }, [listIndex, menuListItems, handleIsClosed, isOpen]);
+
+  useEffect(() => {
+    const handleOutsideClick = (e: MouseEvent) => {
+      if (
+        menuListRef.current &&
+        !menuListRef.current.contains(e.target as Node)
+      ) {
+        handleIsClosed();
+      }
+    };
+    window.addEventListener('click', handleOutsideClick);
+    return () => window.removeEventListener('click', handleOutsideClick);
+  }, [handleIsClosed]);
+
+  return (
+    <nav
+      ref={ref}
+      className={`${
+        isOpen ? 'block' : 'hidden'
+      } absolute top-[30px] right-0 shadow-overlay border rounded sm py-4 px-4 w-[20rem] bg-custom-background_white`}
+      role="menu-nav"
+    >
+      <ul ref={menuListRef} role="menu-list">
+        <li tabIndex={-1} role="menu-list-item" className="first">
+          <a href="#">Link1</a>
+        </li>
+        <li tabIndex={-1} role="menu-list-item" className="second">
+          <a href="#">Link1</a>
+        </li>
+        <li tabIndex={-1} role="menu-list-item" className="third">
+          <a href="#">Link1</a>
+        </li>
+        <li tabIndex={-1} role="menu-list-item" className="four">
+          <a href="#">Link1</a>
+        </li>
+        <li tabIndex={-1} role="menu-list-item" className="fve">
+          <a href="#">Link1</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export const Profile = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const handlePropagation = (e: React.MouseEvent) => {
+    e.stopPropagation();
+    handleIsOpen();
+  };
+  const handleIsOpen = useCallback(() => setIsOpen(true), []);
+  const handleIsClosed = useCallback(() => setIsOpen(false), []);
+
+  return (
+    <section className="relative flex items-center bg-orange-500">
+      <button onClick={handlePropagation}>
+        <User size={25} />
+      </button>
+      <ProfileModal isOpen={isOpen} handleIsClosed={handleIsClosed} />
+    </section>
+  );
+};
