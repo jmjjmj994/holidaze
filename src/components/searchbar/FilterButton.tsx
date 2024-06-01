58738253175b7913c244dcd460e1aef2092fe891
jmjjmj <jonathan.mjos@outlook.com>
2024-06-01 09:35:17 +0200

    added filter button to searchbar

---
+++ src/components/searchbar/FilterButton.tsx
@@ -0,0 +1,8 @@
+import { Faders } from 'phosphor-react';
+export const FilterButton = () => {
+  return (
+    <button className="px-2 py-2 border rounded-sm">
+      <Faders size={25} weight="light" />
+    </button>
+  );
+};
--- src/components/searchbar/SearchBar.tsx
+++ src/components/searchbar/SearchBar.tsx
@@ -1,18 +1,29 @@
 import { MagnifyingGlass } from 'phosphor-react';
+import { FilterButton } from './FilterButton';
 export const SearchBar = () => {
   return (
-    <form className="flex items-center justify-center py-4">
-      <section className="flex items-center justify-center gap-4 max-w-[50rem] w-full">
-        <label htmlFor="search" className="h-full px-2 py-2 border rounded-sm">
-          <MagnifyingGlass aria-label="search icon" />
-        </label>
-        <input
-          id="search"
-          className="w-full px-4 py-1 border rounded-sm"
-          type="text"
-          placeholder="Search"
-        />
-      </section>
-    </form>
+    <section className="flex items-center justify-center  max-w-[50rem] w-full m-auto gap-4">
+      <form className=" py-4 w-full">
+        <section className="flex items-center justify-center gap-4">
+          <label
+            htmlFor="search"
+            className="h-full px-2 py-2 border rounded-sm cursor-pointer"
+          >
+            <MagnifyingGlass
+              size={25}
+              weight="light"
+              aria-label="search icon"
+            />
+          </label>
+          <input
+            id="search"
+            className="w-full px-4 py-2 border rounded-sm"
+            type="text"
+            placeholder="Search"
+          />
+        </section>
+      </form>
+      <FilterButton />
+    </section>
   );
 };
